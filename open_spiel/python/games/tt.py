2423cb886330afe6ee9660a5aaa8f3a504523484
Wilson Wu <wilsoncwu@gmail.com>
2022-01-11 01:30:59 -0500

    kuhn

--- open_spiel/python/games/tt.py
+++ open_spiel/python/games/tt.py
@@ -12,9 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Python implementation of tt.
+# Lint as python3
+"""Kuhn Poker implemented in Python.
 
-This is primarily here to demonstrate simultaneous-move games in Python.
+This is a simple demonstration of implementing a game in Python, featuring
+chance and imperfect information.
+
+Python games are significantly slower than C++, but it may still be suitable
+for prototyping or for small games.
+
+It is possible to run C++ algorithms on Python implemented games, This is likely
+to have good performance if the algorithm simply extracts a game tree and then
+works with that. It is likely to be poor if the algorithm relies on processing
+and updating states as it goes, e.g. MCTS.
 """
 
 import enum
@@ -23,59 +33,48 @@ import numpy as np
 
 import pyspiel
 
-_NUM_PLAYERS = 2
-_DEFAULT_PARAMS = {"termination_probability": 0.125, "max_game_length": 9999}
-_PAYOFF = [[5, 0], [10, 1]]
-
-_GAME_TYPE = pyspiel.GameType(
-    short_name="tt",
-    long_name="TT",
-    dynamics=pyspiel.GameType.Dynamics.SIMULTANEOUS,
-    chance_mode=pyspiel.GameType.ChanceMode.EXPLICIT_STOCHASTIC,
-    information=pyspiel.GameType.Information.PERFECT_INFORMATION,
-    utility=pyspiel.GameType.Utility.GENERAL_SUM,
-    reward_model=pyspiel.GameType.RewardModel.REWARDS,
-    max_num_players=_NUM_PLAYERS,
-    min_num_players=_NUM_PLAYERS,
-    provides_information_state_string=False,
-    provides_information_state_tensor=False,
-    provides_observation_string=False,
-    provides_observation_tensor=False,
-    provides_factored_observation_string=False,
-    parameter_specification=_DEFAULT_PARAMS)
-
 
 class Action(enum.IntEnum):
-  COOPERATE = 0
-  DEFECT = 1
+  PASS = 0
+  BET = 1
 
 
-class Chance(enum.IntEnum):
-  CONTINUE = 0
-  STOP = 1
+_NUM_PLAYERS = 2
+_DECK = frozenset([0, 1, 2])
+_GAME_TYPE = pyspiel.GameType(
+    short_name="tt",
+    long_name="Tiny Titans",
+    dynamics=pyspiel.GameType.Dynamics.SEQUENTIAL,
+    chance_mode=pyspiel.GameType.ChanceMode.EXPLICIT_STOCHASTIC,
+    information=pyspiel.GameType.Information.IMPERFECT_INFORMATION,
+    utility=pyspiel.GameType.Utility.ZERO_SUM,
+    reward_model=pyspiel.GameType.RewardModel.TERMINAL,
+    max_num_players=_NUM_PLAYERS,
+    min_num_players=_NUM_PLAYERS,
+    provides_information_state_string=True,
+    provides_information_state_tensor=True,
+    provides_observation_string=True,
+    provides_observation_tensor=True,
+    provides_factored_observation_string=True)
+_GAME_INFO = pyspiel.GameInfo(
+    num_distinct_actions=len(Action),
+    max_chance_outcomes=len(_DECK),
+    num_players=_NUM_PLAYERS,
+    min_utility=-2.0,
+    max_utility=2.0,
+    utility_sum=0.0,
+    max_game_length=3)  # e.g. Pass, Bet, Bet
 
 
 class TTGame(pyspiel.Game):
-  """The game, from which states and observers can be made."""
+  """A Python version of Tiny Titans."""
 
-  # pylint:disable=dangerous-default-value
-  def __init__(self, params=_DEFAULT_PARAMS):
-    max_game_length = params["max_game_length"]
-    super().__init__(
-        _GAME_TYPE,
-        pyspiel.GameInfo(
-            num_distinct_actions=2,
-            max_chance_outcomes=2,
-            num_players=2,
-            min_utility=np.min(_PAYOFF) * max_game_length,
-            max_utility=np.max(_PAYOFF) * max_game_length,
-            utility_sum=0.0,
-            max_game_length=max_game_length), params)
-    self._termination_probability = params["termination_probability"]
+  def __init__(self, params=None):
+    super().__init__(_GAME_TYPE, _GAME_INFO, params or dict())
 
   def new_initial_state(self):
     """Returns a state corresponding to the start of a game."""
-    return TTState(self, self._termination_probability)
+    return TTState(self)
 
   def make_py_observer(self, iig_obs_type=None, params=None):
     """Returns an object used for observing game state."""
@@ -85,88 +84,86 @@ class TTGame(pyspiel.Game):
 
 
 class TTState(pyspiel.State):
-  """Current state of the game."""
+  """A python version of the tt state."""
 
-  def __init__(self, game, termination_probability):
+  def __init__(self, game):
     """Constructor; should only be called by Game.new_initial_state."""
     super().__init__(game)
-    self._current_iteration = 1
-    self._termination_probability = termination_probability
-    self._is_chance = False
+    self.cards = []
+    self.bets = []
+    self.pot = [1.0, 1.0]
     self._game_over = False
-    self._rewards = np.zeros(_NUM_PLAYERS)
-    self._returns = np.zeros(_NUM_PLAYERS)
+    self._next_player = 0
 
   # OpenSpiel (PySpiel) API functions are below. This is the standard set that
-  # should be implemented by every simultaneous-move game with chance.
+  # should be implemented by every sequential-move game with chance.
 
   def current_player(self):
     """Returns id of the next player to move, or TERMINAL if game is over."""
     if self._game_over:
       return pyspiel.PlayerId.TERMINAL
-    elif self._is_chance:
+    elif len(self.cards) < _NUM_PLAYERS:
       return pyspiel.PlayerId.CHANCE
     else:
-      return pyspiel.PlayerId.SIMULTANEOUS
+      return self._next_player
 
   def _legal_actions(self, player):
     """Returns a list of legal actions, sorted in ascending order."""
     assert player >= 0
-    return [Action.COOPERATE, Action.DEFECT]
+    return [Action.PASS, Action.BET]
 
   def chance_outcomes(self):
     """Returns the possible chance outcomes and their probabilities."""
-    assert self._is_chance
-    return [(Chance.CONTINUE, 1 - self._termination_probability),
-            (Chance.STOP, self._termination_probability)]
+    assert self.is_chance_node()
+    outcomes = sorted(_DECK - set(self.cards))
+    p = 1.0 / len(outcomes)
+    return [(o, p) for o in outcomes]
 
   def _apply_action(self, action):
     """Applies the specified action to the state."""
-    # This is not called at simultaneous-move states.
-    assert self._is_chance and not self._game_over
-    self._current_iteration += 1
-    self._is_chance = False
-    self._game_over = (action == Chance.STOP)
-    if self._current_iteration > self.get_game().max_game_length():
-      self._game_over = True
-
-  def _apply_actions(self, actions):
-    """Applies the specified actions (per player) to the state."""
-    assert not self._is_chance and not self._game_over
-    self._is_chance = True
-    self._rewards[0] = _PAYOFF[actions[0]][actions[1]]
-    self._rewards[1] = _PAYOFF[actions[1]][actions[0]]
-    self._returns += self._rewards
+    if self.is_chance_node():
+      self.cards.append(action)
+    else:
+      self.bets.append(action)
+      if action == Action.BET:
+        self.pot[self._next_player] += 1
+      self._next_player = 1 - self._next_player
+      if ((min(self.pot) == 2) or
+          (len(self.bets) == 2 and action == Action.PASS) or
+          (len(self.bets) == 3)):
+        self._game_over = True
 
   def _action_to_string(self, player, action):
     """Action -> string."""
     if player == pyspiel.PlayerId.CHANCE:
-      return Chance(action).name
+      return f"Deal:{action}"
+    elif action == Action.PASS:
+      return "Pass"
     else:
-      return Action(action).name
+      return "Bet"
 
   def is_terminal(self):
     """Returns True if the game is over."""
     return self._game_over
 
-  def rewards(self):
-    """Reward at the previous step."""
-    return self._rewards
-
   def returns(self):
     """Total reward for each player over the course of the game so far."""
-    return self._returns
+    pot = self.pot
+    winnings = float(min(pot))
+    if not self._game_over:
+      return [0., 0.]
+    elif pot[0] > pot[1]:
+      return [winnings, -winnings]
+    elif pot[0] < pot[1]:
+      return [-winnings, winnings]
+    elif self.cards[0] > self.cards[1]:
+      return [winnings, -winnings]
+    else:
+      return [-winnings, winnings]
 
   def __str__(self):
     """String for debug purposes. No particular semantics are required."""
-    return (f"p0:{self.action_history_string(0)} "
-            f"p1:{self.action_history_string(1)}")
-
-  def action_history_string(self, player):
-    return "".join(
-        self._action_to_string(pa.player, pa.action)[0]
-        for pa in self.full_history()
-        if pa.player == player)
+    return "".join([str(c) for c in self.cards] + ["pb"[b] for b in self.bets])
 
 
 class TTObserver:
@@ -174,21 +171,55 @@ class TTObserver:
 
   def __init__(self, iig_obs_type, params):
     """Initializes an empty observation tensor."""
-    assert not bool(params)
-    self.iig_obs_type = iig_obs_type
-    self.tensor = None
+    if params:
+      raise ValueError(f"Observation parameters not supported; passed {params}")
+
+    # Determine which observation pieces we want to include.
+    pieces = [("player", 2, (2,))]
+    if iig_obs_type.private_info == pyspiel.PrivateInfoType.SINGLE_PLAYER:
+      pieces.append(("private_card", 3, (3,)))
+    if iig_obs_type.public_info:
+      if iig_obs_type.perfect_recall:
+        pieces.append(("betting", 6, (3, 2)))
+      else:
+        pieces.append(("pot_contribution", 2, (2,)))
+
+    # Build the single flat tensor.
+    total_size = sum(size for name, size, shape in pieces)
+    self.tensor = np.zeros(total_size, np.float32)
+
+    # Build the named & reshaped views of the bits of the flat tensor.
     self.dict = {}
+    index = 0
+    for name, size, shape in pieces:
+      self.dict[name] = self.tensor[index:index + size].reshape(shape)
+      index += size
 
   def set_from(self, state, player):
-    pass
+    """Updates `tensor` and `dict` to reflect `state` from PoV of `player`."""
+    self.tensor.fill(0)
+    if "player" in self.dict:
+      self.dict["player"][player] = 1
+    if "private_card" in self.dict and len(state.cards) > player:
+      self.dict["private_card"][state.cards[player]] = 1
+    if "pot_contribution" in self.dict:
+      self.dict["pot_contribution"][:] = state.pot
+    if "betting" in self.dict:
+      for turn, action in enumerate(state.bets):
+        self.dict["betting"][turn, action] = 1
 
   def string_from(self, state, player):
     """Observation of `state` from the PoV of `player`, as a string."""
-    if self.iig_obs_type.public_info:
-      return (f"us:{state.action_history_string(player)} "
-              f"op:{state.action_history_string(1 - player)}")
-    else:
-      return None
+    pieces = []
+    if "player" in self.dict:
+      pieces.append(f"p{player}")
+    if "private_card" in self.dict and len(state.cards) > player:
+      pieces.append(f"card:{state.cards[player]}")
+    if "pot_contribution" in self.dict:
+      pieces.append(f"pot[{int(state.pot[0])} {int(state.pot[1])}]")
+    if "betting" in self.dict and state.bets:
+      pieces.append("".join("pb"[b] for b in state.bets))
+    return " ".join(str(p) for p in pieces)
 
 
 # Register the game with the OpenSpiel library
